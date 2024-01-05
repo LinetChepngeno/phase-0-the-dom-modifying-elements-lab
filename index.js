6c09d85d1a80f9a2bfec958b74fcbd87fd17c51f
Linet Chepngeno <linetmutai@gmail.com>
2024-01-05 10:50:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+//Code to remove the node element here
+document.querySelector("main#main").remove();
+//Creating a new element and then assign it to the newHeader
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Linet Chepngeno is the champion";
